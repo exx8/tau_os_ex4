cda0310ef6f8db4f4c02713fd4fa79f75bd272d3
exx8 <exx8eran@gmail.com>
2020-12-28 21:28:05 +0200

    .

--- pfind.c
+++ pfind.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
+typedef struct _QueueNode
+{
+char* path[PATH_MAX];
+struct _QueueNode* next;
+}QueueNode;
 int main(int argc, const char* argv[]) {
     const char* root=argv[1];
     const char* term=argv[2];
