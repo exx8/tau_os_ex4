614811e0c503b175176c5126c9c626d119f97baa
exx8 <exx8eran@gmail.com>
2020-12-28 20:27:09 +0200

    .

--- pfind.c
+++ pfind.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, const char* argv[]) {
+    const char* root=argv[1];
+    const char* term=argv[2];
+    const int thread_num=atoi(argv[3]);
+
     printf("Hello, World!\n");
     return 0;
 }
