e5a67a9e22aea6f968004c57b17463f8c79090e3
exx8 <exx8eran@gmail.com>
2020-12-28 21:35:14 +0200

    .

--- pfind.c
+++ pfind.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <limits.h>
 
-typedef struct _QueueNode
-{
-char* path[PATH_MAX];
-struct _QueueNode* next;
-}QueueNode;
-int main(int argc, const char* argv[]) {
-    const char* root=argv[1];
-    const char* term=argv[2];
-    const int thread_num=atoi(argv[3]);
+typedef struct _QueueNode {
+    char *path[PATH_MAX];
+    struct _QueueNode *next;
+} QueueNode;
+
+int main(int argc, const char *argv[]) {
+    const char *root = argv[1];
+    const char *term = argv[2];
+    const int thread_num = atoi(argv[3]);
 
     printf("Hello, World!\n");
     return 0;
